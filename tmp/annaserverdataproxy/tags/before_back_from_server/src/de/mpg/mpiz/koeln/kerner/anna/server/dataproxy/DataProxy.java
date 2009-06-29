bf1fb36c4b360df0696a1c2ad6a925918d328e99
2407@gmx.org <2407@gmx.org@5bb82204-3bbe-11de-801c-c7cb65f9a556>
2009-06-29 15:07:23 +0000

    one last tag before making data handling own osgi service again.

---
+++ tmp/annaserverdataproxy/tags/before_back_from_server/src/de/mpg/mpiz/koeln/kerner/anna/server/dataproxy/DataProxy.java
@@ -0,0 +1,12 @@
+package de.mpg.mpiz.koeln.kerner.anna.server.dataproxy;
+
+import de.mpg.mpiz.koeln.kerner.anna.server.dataproxy.data.DataBean;
+import de.mpg.mpiz.koeln.kerner.anna.server.dataproxy.data.DataBeanAccessException;
+
+
+public interface DataProxy {
+
+	DataBean getDataBean() throws DataBeanAccessException;
+	void updateDataBean(DataBean dataBean) throws DataBeanAccessException;
+
+}
---
+++ tmp/annaserverdataproxy/tags/before_back_from_server/src/de/mpg/mpiz/koeln/kerner/anna/server/dataproxy/dataimpl/DataProxyActivator.java
@@ -0,0 +1,37 @@
+package de.mpg.mpiz.koeln.kerner.anna.server.dataproxy.dataimpl;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import de.mpg.mpiz.koeln.kerner.anna.server.dataproxy.DataProxy;
+import de.mpg.mpiz.koeln.kerner.anna.server.dataproxyimpl.DataProxyImpl;
+
+public class DataProxyActivator implements BundleActivator {
+
+	// public static LogDispatcher LOGGER = null;
+
+	@SuppressWarnings("unchecked")
+	public void start(BundleContext context) throws Exception {
+		// TODO remove try catch
+		try {
+			// LOGGER = new LogDispatcher(context);
+			DataProxy service = new DataProxyImpl();
+			context.registerService(DataProxy.class.getName(), service,
+					new Hashtable());
+			System.out.println(this + " activated");
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO method stub
+	}
+	
+	public String toString(){
+		return this.getClass().getSimpleName();
+	}
+
+}
