4c8f8b7da0ab302eaea31e7e2b25c23caac7d68c
2407@gmx.org <2407@gmx.org@5bb82204-3bbe-11de-801c-c7cb65f9a556>
2009-08-17 09:19:35 +0000

    initial import. DataBean was moved away from dataproxy bundle to own bundle

---
+++ tmp/annaserverdata/trunk/src/de/mpg/mpiz/koeln/anna/server/data/DataBeanAccessException.java
@@ -0,0 +1,26 @@
+package de.mpg.mpiz.koeln.anna.server.data;
+
+public class DataBeanAccessException extends Exception {
+
+	private static final long serialVersionUID = 4002480662305683330L;
+
+	public DataBeanAccessException() {
+		
+	}
+
+	public DataBeanAccessException(String arg0) {
+		super(arg0);
+		
+	}
+
+	public DataBeanAccessException(Throwable arg0) {
+		super(arg0);
+		
+	}
+
+	public DataBeanAccessException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+}
